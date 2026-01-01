4e5f30c9e9cdce4d404df810a42eb8b12c1f5da5
rupesh0001-tech <rupeshcoding01@gmail.com>
2026-01-01 12:28:10 +0530

    setup authh middleware

---
+++ backend/middlewares/user.auth.middlewares.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
+
+interface JwtPayload {
+  _id: string;
+}
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // get token from cookie
+    const token = req.cookies?.token;
+
+    // validate token
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    // verify token
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    // attach user id to req
+    req.userId = decoded._id;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
