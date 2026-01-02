f2fe7997231fcf839e8db52b68a1905d09500753
rupesh0001-tech <rupeshcoding01@gmail.com>
2026-01-02 10:33:34 +0530

    made fucntions for resgister and login the user

---
+++ frontend/src/functions/AuthUser.ts
@@ -0,0 +1,28 @@
+import { api } from "../api/api";
+import type { LoginUserI, RegisterUserI } from "../interfaces/interfaces";
+
+export const registerUser = async ({ user }: { user: RegisterUserI }) => {
+  try {
+    const res = await api.post(
+      "/api/users/register",
+      user,
+      { withCredentials: true }
+    );
+    return res.data; 
+  } catch (error: any) {
+    throw error.response?.data || error; // ✅ let caller handle error
+  }
+};
+
+export const loginUser = async ({ user }: { user: LoginUserI }) => {
+  try {
+    const res = await api.post(
+      "/api/users/login",
+      user,
+      { withCredentials: true }
+    );
+    return res.data; 
+  } catch (error: any) {
+    throw error.response?.data || error; // ✅ let caller handle error
+  }
+};
