dd68ca1abc7e8f61b2b2de9e75fcc9295872f54a
rupesh0001-tech <rupeshcoding01@gmail.com>
2026-01-02 14:11:41 +0530

    integrated the current user with navbar

--- frontend/src/Components/Home/MainChat/ChatNavbar.tsx
+++ frontend/src/Components/Home/MainChat/ChatNavbar.tsx
@@ -1,14 +1,19 @@
+import { useContext } from "react";
+import { MyContext } from "../../../Context/ContextMenu";
+
 const ChatNavbar = () => {
-  
+  const { currentUser } = useContext(MyContext);
   return (
-    <div className="h-14 flex items-center px-4 border-b bg-white">
+    
+      <div className="h-14 flex items-center px-4 border-b bg-white">
       <div className="flex items-center gap-3">
         <div className="h-9 w-9 rounded-full bg-blue-500 text-white flex items-center justify-center font-semibold">
-          A
+          {currentUser?.name?.charAt(0).toUpperCase()}
         </div>
-        <span className="font-medium">Aayush</span>
+        <span className="font-medium">{ currentUser?.name}</span>
       </div>
     </div>
+    
   );
 };
 
