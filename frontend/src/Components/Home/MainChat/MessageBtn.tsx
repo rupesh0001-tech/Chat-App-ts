034c94b0fe9b5454855df9c93f2f4769b651d899
rupesh0001-tech <rupeshcoding01@gmail.com>
2026-01-02 19:05:06 +0530

    debugging

--- frontend/src/Components/Home/MainChat/MessageBtn.tsx
+++ frontend/src/Components/Home/MainChat/MessageBtn.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { MyContext } from "../../../Context/ContextMenu";
+
 const MessageBtn = ({ message }: any) => {
+  const { user } = useContext(MyContext);
+  console.log(message)
   return (
     <div
       className={`max-w-[75%] px-6 py-3 font-semibold rounded-xl text-md ${
-        message.isMe
+        message.sender === user?._id
           ? "self-end bg-blue-500 text-white"
           : "self-start bg-gray-100 text-black"
       }`}
